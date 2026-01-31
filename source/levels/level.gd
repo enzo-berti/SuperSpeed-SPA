76f2188addd72e5d1fe0313a55264d5d1e3e2cf0
reilpahc <101335252+reilpahc@users.noreply.github.com>
2026-01-31 19:43:29 +0100

    Cucumber minigame update + ease script

--- source/levels/level.gd
+++ source/levels/level.gd
@@ -7,22 +7,22 @@ var is_there_client : bool = false
 ###### BUILT-IN FUNCTIONS ######
 
 func _ready() -> void:
-    spawn_client()
-    pass
+	spawn_client()
+	pass
 
 func _process(delta: float) -> void:
-    if !is_there_client:
-        spawn_client()
-    pass
+	if !is_there_client:
+		spawn_client()
+	pass
 
 
 ###### CUSTOM FUNCTIONS ######
 
 #Spawn new client
 func spawn_client() -> void:
-    var client = preload("res://characters/client/client.tscn")
-    var instance = client.instantiate()
-    add_child(instance)
-    instance.position = spawn_pos
-    is_there_client = true
-    pass
+	var client = preload("res://characters/client/client.tscn")
+	var instance = client.instantiate()
+	add_child(instance)
+	instance.position = spawn_pos
+	is_there_client = true
+	pass
