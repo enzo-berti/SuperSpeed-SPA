30d1492e703ead92b41e1eb9ed3c671db3a8e1c2
PeterLeBagarreur <dagneauxalois@gmail.com>
2026-01-31 19:47:05 +0100

    MainMenu
    Création de la Scène
    Animation des Boutons
    Boutons fonctionnels

---
+++ source/levels/MenuPrincipal/menu_principal.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@export var tween_intensity: float
+@export var tween_duration: float
+
+@onready var StartButton: Button = $VBoxContainer/StartButton
+@onready var QuitButton: Button = $VBoxContainer/QuitButton
+@onready var level = preload("res://levels/level.tscn")
+
+func _process(delta: float) -> void:
+	btn_hovered(StartButton)
+	btn_hovered(QuitButton)
+
+func start_tween(object: Object, property: String, final_val: Variant, duration: float):
+	var tween = create_tween()
+	tween.tween_property(object, property, final_val, duration)
+
+func btn_hovered(button: Button):
+	button.pivot_offset = button.size/2
+	if button.is_hovered():
+		start_tween(button, "scale", Vector2.ONE*tween_intensity, tween_duration)
+	else:
+		start_tween(button, "scale", Vector2.ONE, tween_duration)
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_packed(level)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
