1d217696b111a5527e329692c18fc9ef2464e395
reilpahc <101335252+reilpahc@users.noreply.github.com>
2026-02-01 09:18:49 +0100

    Cucumber spawn on eyes pos
    Conflict between previous cucumber input and new one

--- source/mini_games/cucumber/cucumber.gd
+++ source/mini_games/cucumber/cucumber.gd
@@ -1,42 +1,53 @@
 extends Node2D
 
-#Exported nodes
-@export var path_follow : PathFollow2D
-@export var sprite : Sprite2D
+#Nodes
+@onready var eye_pos : Vector2 = position
+@onready var cucumber_pos : Vector2 = $PathFollow2D.position
+@onready var path_follow : PathFollow2D = $PathFollow2D
+@onready var sprite : Sprite2D = $"../Sprite2D"
 
-@export var debug_score_distance_limit : CollisionShape2D
-@export var debug_score_distance_deadzone : CollisionShape2D
+@onready var debug_score_distance_limit : CollisionShape2D = $PathFollow2D/DebugScoreDistanceLimit
+@onready var debug_score_distance_deadzone : CollisionShape2D = $PathFollow2D/DebugScoreDistanceDeadzone
+@onready var debug_state : Label = $"../DebugState"
+
+#State machine
+var states : Array[String] = ["move", "stopped", "scored"]
+var state_machine : String = states[0]
 
 #Cucumber movement
 var x : float = 0
 var direction_reversed : bool = false
-var is_stopped : bool = false
 
 #Score variables
-@onready var parent_eye : Node2D = get_parent()
 @onready var score_distance_limit : float = debug_score_distance_limit.shape.radius
 @onready var score_distance_dead_zone : float = debug_score_distance_deadzone.shape.radius
 @export var score_max : int = 25
 var score : int
+
+signal cucumber_stopped(target_misses : bool)
 
 
 ###### BUILT-IN FUNCTIONS ######
 
 
 func _process(delta: float) -> void:
+	cucumber_pos = $PathFollow2D.position
+	
+	print(cucumber_pos, " + ", eye_pos)
 	
 	#Cucumber movement
-	match is_stopped:
-		false:
+	match state_machine:
+		"move":
 			_slide(delta)
-		true:
-			score = int(score_max * (1 - clamp(sprite.global_position.distance_to(parent_eye.global_position) - score_distance_dead_zone, 0.0, score_distance_limit) / score_distance_limit))
-			print(score)
-			pass
-
-	#Stop
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && not is_stopped:
-		is_stopped = true
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+				state_machine = states[1]
+		"stopped":
+			_score()
+			state_machine = states[2]
+		"scored":
+			queue_free()
+		
+	debug_state.text = state_machine
 
 
 ###### CUSTOM FUNCTIONS ######
@@ -55,3 +66,13 @@ func _slide(delta: float) -> void:
 		direction_reversed = false
 	
 	path_follow.progress_ratio = ease.InOutSine(x)
+	sprite.position = cucumber_pos
+
+func _score() -> void:
+	score = int(score_max * (1 - clamp(cucumber_pos.distance_to(eye_pos) - score_distance_dead_zone, 0.0, score_distance_limit) / score_distance_limit))
+	print(score)
+	
+	if score == 0:
+		cucumber_stopped.emit(true)
+	if score > 0:
+		cucumber_stopped.emit(false)
---
+++ source/mini_games/cucumber/cucumber_game.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var eye_turn : int = 0
+@onready var eyes : Array[Node2D]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	eyes.append_array(get_tree().get_nodes_in_group("eyes"))
+	_spawn_cucumber()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+###### CUSTOM FUNCTIONS ######
+
+func _spawn_cucumber() -> void:
+	var client = preload("res://mini_games/cucumber/cucumber.tscn")
+	var instance = client.instantiate()
+	add_child(instance)
+	instance.position = eyes[eye_turn].position
+	instance.find_child("Path2D").cucumber_stopped.connect(_on_cucumber_stopped)
+
+
+###### SIGNAL FUNCTIONS ######
+
+func _on_cucumber_stopped(target_missed) -> void:
+	print(target_missed)
+	if target_missed:
+		_spawn_cucumber()
+	elif not target_missed && eyes.size() - 1 != eye_turn:
+		eye_turn += 1
+		_spawn_cucumber()
