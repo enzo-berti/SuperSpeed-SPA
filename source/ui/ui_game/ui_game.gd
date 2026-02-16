90c32a0682049bc226e3c80b72a0a4ee6c02aa26
EnzoTatou <enzoberti@protonmail.com>
2026-02-16 12:47:01 +0100

    Update life and patience meter
    - Fix patience meter displaying incorrect value
    - Make the life more dynamic
    - Clean ui_game.gd script

--- source/ui/ui_game/ui_game.gd
+++ source/ui/ui_game/ui_game.gd
@@ -1,15 +1,15 @@
 extends Control
 
-@export var timer : Timer
-@export var star_timer : Timer
-@export var patience_time : float = 20.0
+@export var patience_timer : Timer
+@export var star_timer: Timer
+@export var patience_time : float = 50.0
+@export var patience_time_min : float = 20.0
 @export var patience_meter : TextureProgressBar
 
 @export var score_label : LabelScore
 
 @onready var stars_array : Array[TextureProgressBar] = [$HeartContainer/Star1, $HeartContainer/Star2, $HeartContainer/Star3]
 
-var is_client_angry : bool = false
 var x : float
 
 signal patience_timeout
@@ -20,60 +20,54 @@ signal patience_timeout
 func _ready() -> void:
 	patience_meter.visible = false
 
-
 func _process(delta: float) -> void:
-	patience_meter.value = timer.time_left / clampf((patience_time - (5 * GameManager.win_clients)), 20, patience_time) * 100
+	patience_meter.value = patience_timer.time_left / patience_timer.wait_time
 	
 	if GameManager.health <= 0:
 		get_tree().change_scene_to_packed(load(GameManager.GAME_OVER_SCENE_UID))
 	
 	score_label.text_desired = str(GameManager.score)
-	#if is_client_angry:
-		#x = clampf(star_timer.time_left, 0.0, 1.0)
-		#stars_array[GameManager.health].value = ease.OutBounce(x)
-		#is_client_angry = false
 	
-	
-	#if Input.is_action_just_pressed("ui_accept"):
-		#start_patience()
-	#if Input.is_action_just_pressed("MOUSE_BUTTON_LEFT"):
-		#angry_client(5.0)
+	if !star_timer.is_stopped():
+		print(star_timer.time_left)
+		stars_array[GameManager.health].value = star_timer.time_left / star_timer.wait_time
 
 
 ###### CUSTOM FUNCTIONS ######
-
 func start_patience() -> void:
-	timer.wait_time = patience_time - (5 * GameManager.win_clients)
+	var patience_time_desired: float = patience_time - (5 * GameManager.win_clients)
+	patience_time = max(patience_time_desired, patience_time_min)
+	
+	patience_timer.wait_time = patience_time
 	patience_meter.visible = true
-	timer.start()
+	patience_timer.start()
 
 func stop_patience() -> void:
 	patience_meter.visible = false
-	timer.stop()
+	patience_timer.stop()
 
 func angry_client(time_malus : int) -> void:
-	var remaining_time : float = timer.time_left
+	var remaining_time : float = patience_timer.time_left
 	
 	if remaining_time <= time_malus:
-		timer.stop()
-		timer.wait_time = 0.1
-		timer.start()
+		patience_timer.stop()
+		patience_timer.wait_time = 0.1
+		patience_timer.start()
 		return
 	
-	timer.stop()
-	timer.wait_time = remaining_time - time_malus
-	timer.start()
+	patience_timer.stop()
+	patience_timer.wait_time = remaining_time - time_malus
+	patience_timer.start()
 
 
 ###### SIGNAL FUNCTIONS ######
 
 func _on_timer_timeout() -> void:
 	GameManager.win_strike -= 1
-	#is_client_angry = true
-	#star_timer.start()
 	GameManager.health -= 1
-	stars_array[GameManager.health].value = 0.0
+	
+	star_timer.start()
 	patience_meter.visible = false
-	timer.stop()
+	patience_timer.stop()
 	patience_timeout.emit()
 	stop_patience()
